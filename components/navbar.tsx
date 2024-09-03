6c8572c696165e91cf634caaeb46cc2fc7c747ae
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-09-04 00:00:04 +0300

    Refactor navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,6 @@
-import { auth, currentUser } from '@clerk/nextjs/server';
-import SignIn from './sign-in';
-import SignOut from "./sign-out";
-
 export default async function Navbar() {
-  const { userId } = auth();
-
   return (
     <nav>
-      {userId ? <SignOut /> : <SignIn />}
     </nav>
   );
 };
