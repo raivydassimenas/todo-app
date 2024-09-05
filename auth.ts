7c44dc68e84b2d97c95f005238b024d402738cec
Raivydas Šimėnas <raivydas.simenas@proton.me>
2024-09-05 21:27:23 +0300

    Add partial Credentials authentication support

--- auth.ts
+++ auth.ts
@@ -1,8 +1,41 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { prisma } from "@/prisma"
+import NextAuth, { type DefaultSession } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "@/prisma";
+import bcrypt from "bcryptjs";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      address: string;
+    } & DefaultSession["user"];
+  }
+};
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
-})
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const user = await prisma.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user) {
+          throw new Error("No user found");
+        }
+        const comp = await bcrypt.compare(credentials.password, user.password);
+        if (comp) {
+          return user;
+        } else {
+          throw new Error("Password does not match");
+        }
+      },
+    }),
+  ],
+});
